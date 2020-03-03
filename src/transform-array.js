022d96fdcbc93c404b3431223767476141cba2e6
yafimchik <yafimenka.jk@gmail.com>
2020-03-04 02:53:24 +0300

    feat: solve transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,76 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    try{
+        if (arr == undefined) throw TypeError("undefined");
+        if (!(arr instanceof Array)) throw TypeError("not an array");
+        
+        //return arr;
+        let i = 0;
+
+        while (i < arr.length){
+       
+                
+            if (arr[i] == "--double-prev"){
+                  
+                if (i == 0) {
+                    arr.splice(i,1);
+                        
+                    continue;
+                }
+                arr.splice(i, 1, arr[i - 1]);
+                i--;
+                continue;
+            }
+            if (arr[i] == "--double-next"){
+                  
+                if (i == arr.length - 1) {
+                    arr.splice(i,1);
+                        
+                    continue;
+                }
+                arr.splice(i, 1,arr[i + 1] );
+                    
+                continue;
+            }
+            
+            i++;
+
+        }
+        i = 0;
+        while (i < arr.length){
+       
+                
+            if (arr[i] == "--discard-prev"){
+                if (i == 0) {
+                    arr.splice(i,1);
+                        
+                    continue;
+                }
+                arr.splice(i - 1, 2);
+                i--;
+                continue;
+            }
+            if (arr[i] == "--discard-next"){
+                  
+                if (i == arr.length - 1) {
+                    arr.splice(i,1);
+                        
+                    continue;
+                }
+                arr.splice(i, 2);
+                    
+                continue;
+            }
+                
+            i++;
+
+        }
+
+        return arr;
+       
+           
+    }
+    catch (err){
+        alert(err);
+        
+    }
 };
