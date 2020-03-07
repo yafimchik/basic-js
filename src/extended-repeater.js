015126ac5d78266455b8b6748d6bef9be900bbdc
yafimchik <yafimenka.jk@gmail.com>
2020-03-07 20:24:32 +0300

    feat: add solve of extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,39 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    if (typeof(options) != "object") return str;
+    let repstr = String(str);
+    let sepstr = "+";
+    let addstr = "";
+    let sepadd = "|";
+    let rept = 0;
+    let addt = 0;
+    let resStr = "";
+    if ("repeatTimes" in options){
+        if (options["repeatTimes"] === undefined) rept = 1;
+        else rept = Number(options["repeatTimes"]);        
+    }
+    if ("separator" in options){
+        sepstr = String(options["separator"]);        
+    }
+    if ("addition" in options){
+        addstr = String(options["addition"]);        
+    }
+    if ("additionRepeatTimes" in options){
+        if (options["additionRepeatTimes"] === undefined) addt = 1;
+        else addt = Number(options["additionRepeatTimes"]);       
+    }
+    if ("additionSeparator" in options){
+        sepadd = String(options["additionSeparator"]);        
+    }
+    
+    for (let i = 0; i < rept; i++){
+        if (i != 0) resStr += sepstr;
+        resStr += repstr;    
+        for (let j = 0; j < addt; j++){
+            if (j != 0) resStr += sepadd;    
+            resStr += addstr;
+        }
+    }
+    return resStr;
+    
 };
   
