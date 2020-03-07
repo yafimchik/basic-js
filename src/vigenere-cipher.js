b630eb17c38cfa1a4ca2a17aa426a5fb6bd3cdf4
yafimchik <yafimenka.jk@gmail.com>
2020-03-07 22:10:40 +0300

    feat: add solve of vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,72 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    
+    constructor(direct = true){
+        if (direct === false) this.isReverse = true;
+        else this.isReverse = false;       
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        let alf = "abcdefghijklmnopqrstuvwxyz";
+        alf = alf.toUpperCase();
+        let indexFLetter = alf[0].charCodeAt();
+        if (typeof(message)===undefined) throw error;
+        if (typeof(key)===undefined) throw error;
+        let result = "";
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+      
+        let j = 0;
+        for (let i = 0; i < message.length; i++){
+            if (alf.indexOf(message[i]) == -1){
+                result += message[i];
+                continue;
+            }
+            let indexM = alf.indexOf(message[i]) + indexFLetter;
+            let indexK = alf.indexOf(key[j]) + indexFLetter;
+            result += alf[(indexK + indexM) % alf.length];
+            j++;
+            if (j == key.length) j = 0;
+        }
+        
+        if (this.isReverse) {
+            let tmp = result;
+            result = "";
+            for (let i = 0; i < tmp.length; i++){
+                result = tmp[i] + result;
+            }
+        }
+        return result;
+    }
+
+    decrypt(message, key)  {
+        let alf = "abcdefghijklmnopqrstuvwxyz";
+        alf = alf.toUpperCase();
+        let indexFLetter = alf[0].charCodeAt();
+        if (typeof(message)===undefined) throw error;
+        if (typeof(key)===undefined) throw error;
+        let result = "";
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        let j = 0;
+        for (let i = 0; i < message.length; i++){
+            if (alf.indexOf(message[i]) == -1){
+                result += message[i];
+                continue;
+            }
+            let indexM = alf.indexOf(message[i]) + indexFLetter;
+            let indexK = alf.indexOf(key[j]) + indexFLetter;
+            result += alf[(alf.length - indexK + indexM) % alf.length];
+            j++;
+            if (j == key.length) j = 0;
+        }
+        if (this.isReverse) {
+            let tmp = result;
+            result = "";
+            for (let i = 0; i < tmp.length; i++){
+                result = tmp[i] + result;
+            }
+        }
+        return result;
     }
 }
 
